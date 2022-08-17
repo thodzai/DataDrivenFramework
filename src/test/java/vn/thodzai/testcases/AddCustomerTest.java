f4b3d069a2c182e81ef18c6facc2e13f8a1cb901
phanvantho <tho.phanv@homecredit.vn>
2022-08-17 22:30:49 +0700

    Update

--- src/test/java/vn/thodzai/testcases/AddCustomerTest.java
+++ src/test/java/vn/thodzai/testcases/AddCustomerTest.java
@@ -1,6 +1,9 @@
 package vn.thodzai.testcases;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import vn.thodzai.base.TestBase;
@@ -8,13 +11,18 @@ import vn.thodzai.base.TestBase;
 public class AddCustomerTest extends TestBase {
 
     @Test(dataProvider = "getData")
-    public void addCustomer(String firstName, String lastName, String postCode) {
+    public void addCustomer(String firstName, String lastName, String postCode, String popUpMessage) throws InterruptedException {
 
+        webDriver.findElement(By.cssSelector(OR.getProperty("bankManagerLoginBtn_CSS"))).click();
         webDriver.findElement(By.cssSelector(OR.getProperty("addCustomerBtn_CSS"))).click();
         webDriver.findElement(By.cssSelector(OR.getProperty("firstNameTxt_CSS"))).sendKeys(firstName);
         webDriver.findElement(By.xpath(OR.getProperty("lastNameTxt_XPATH"))).sendKeys(lastName);
         webDriver.findElement(By.cssSelector(OR.getProperty("postCodeTxt_CSS"))).sendKeys(postCode);
         webDriver.findElement(By.cssSelector(OR.getProperty("addCustomerSubmitBtn_CSS"))).click();
+
+        Alert alert = webDriverWait.until(ExpectedConditions.alertIsPresent());
+        Assert.assertTrue(alert.getText().contains(popUpMessage));
+        alert.accept();
 
     }
 
@@ -28,13 +36,15 @@ public class AddCustomerTest extends TestBase {
 
         Object[][] data = new Object[rows - 1][cols];
 
-        for (int rowNum = 2; rowNum < rows; rowNum++) {
+        for (int rowNum = 2; rowNum <= rows; rowNum++) {
             for (int colNum = 0; colNum < cols; colNum++) {
 
                 data[rowNum - 2][colNum] = excelReader.getCellData(sheetName, colNum, rowNum);
 
             }
         }
+
+        return data;
 
     }
 
